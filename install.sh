83230e825a0f93d3a1f9963b5f07ad4269a43a89
Strider <avj14566@zamge.com>
2020-04-24 15:34:49 +0200

    update// added installer and uninstaller

--- install.sh
+++ install.sh
@@ -4,12 +4,26 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-cp kali-undercover.sh /usr/bin/kali-undercover
-chmod 755 /usr/bin/kali-undercover
-cp -r kali-undercover /usr/share/kali-undercover
-chmod +x /usr/share/kali-undercover/scripts/xfce4-desktop-profiles.py
+echo "Copy undercover.sh to /usr/bin/undercover ..."
+cp undercover.sh /usr/bin/undercover
+echo "Set /usr/bin/undercover to 755 ..."
+chmod 755 /usr/bin/undercover
+echo "Copy undercover/ to /usr/share/undercover ..."
+cp -r undercover /usr/share/undercover
+echo "Set /usr/bin/undercover/scripts/xfce4-desktop-profiles.py to 755 ..."
+chmod 755 /usr/share/undercover/scripts/xfce4-desktop-profiles.py
+echo "Copy Windows-10 to /usr/share/themes/Windows-10 ..."
 cp -r Windows-10 /usr/share/themes/Windows-10
+echo "Set /usr/share/themes/Windows-10 to 755 ..."
 chmod 755 -R /usr/share/themes/Windows-10
+echo "Copy Windows-10-Icons to /usr/share/icons/Windows-10-Icons ..."
 cp -r Windows-10-Icons /usr/share/icons/Windows-10-Icons
+echo "Set /usr/share/icons/Windows-10-Icons to 755 ..."
 chmod 755 -R /usr/share/icons/Windows-10-Icons
+
+
+echo "Adding windows banner to ~/.bashrc"
+printf 'print_win_banner() {\necho "M1cr0s0ft W1nd0ws [Version 10.0.17763.55]" \necho "(c) 2018 M1cr0s0ft C0rp0r4ti0n. All rights reserved." \necho "" \n }\n' >> ~/.bashrc
+printf '#print_win_banner;' >> ~/.bashrc
+
 echo "Ready"
--- uninstall.sh
+++ uninstall.sh
@@ -4,8 +4,23 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-rm /usr/bin/kali-undercover
-rm -rf /usr/share/kali-undercover
+echo "Remove /usr/bin/undercover ..."
+rm /usr/bin/undercover
+echo "Remove /usr/share/undercover ..."
+rm -rf /usr/share/undercover
+echo "Remove /usr/share/theme/Windows-10 ..."
 rm -rf /usr/share/themes/Windows-10
+echo "Remove /usr/share/icons/Windows-10-Icons ..."
 rm -rf /usr/share/icons/Windows-10-Icons
+
+echo "Removing windows banner from ~/.bashrc"
+
+sed -i -e 's/print_win_banner() {//g' ~/.bashrc
+sed -i -e 's/echo \"M1cr0s0ft W1nd0ws \[Version 10.0.17763.55\]\" //g' ~/.bashrc
+sed -i -e 's/echo \"(c) 2018 M1cr0s0ft C0rp0r4ti0n. All rights reserved.\" //g' ~/.bashrc
+sed -i -e 's/echo \"\" //g' ~/.bashrc
+sed -i -e 's/ \}//g' ~/.bashrc
+sed -i -e 's/print_win_banner;//g' ~/.bashrc
+sed -i -e 's/#print_win_banner;//g' ~/.bashrc
+
 echo "Ready"
